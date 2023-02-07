135539c8ac2a9da2378abd01d88fc1631f6e9f54
dorteks <ayodelealayemi@gmail.com>
2023-02-07 11:20:11 +0100

    added responsive elements to topbar component

--- src/layout/topbar/index.tsx
+++ src/layout/topbar/index.tsx
@@ -1,9 +1,42 @@
-// import Sidebar from "../sidebar";
+import SearchButton from "@/components/searchButton";
 import React from "react";
 import styles from "./topbar.module.scss";
 
 const Topbar = () => {
-  return <main className={styles.container}>Topbar</main>;
+  return (
+    <main className={styles.container}>
+      <div className={styles.openMenu}>
+        <img src="/images/hamburger.png" />
+      </div>
+
+      <img className={styles.logo} src="/images/logo.svg" alt="logo" />
+
+      <SearchButton placeholder="Search for anything" />
+
+      <h5 className={styles.docs}>Docs</h5>
+
+      <img
+        alt="notification icon"
+        src="/images/notification.svg"
+        className={styles.notification}
+      />
+
+      <div></div>
+      <img
+        alt="user profile picture"
+        src="/images/user-avatar.svg"
+        className={styles.userAvatar}
+      />
+
+      <h5 className={styles.userName}>Ayodele</h5>
+
+      <img
+        alt="dropdown menu icon"
+        src="/images/arrow-down.svg"
+        className={styles.arrowDown}
+      />
+    </main>
+  );
 };
 
 export default Topbar;
