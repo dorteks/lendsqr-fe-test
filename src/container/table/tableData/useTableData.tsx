b4b926e6f58b48df2aef47580df3815eb05a9ad6
dorteks <ayodelealayemi@gmail.com>
2023-02-09 23:41:37 +0100

    changed API for mock data

--- src/container/table/tableData/useTableData.tsx
+++ src/container/table/tableData/useTableData.tsx
@@ -2,10 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 // Mock data API
-// "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users"
+// "http://localhost:4000/users"
 
 const fetchTableData = async () => {
-  const res = await axios.get("http://localhost:4000/users");
+  const res = await axios.get(
+    "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users"
+  );
   return res;
 };
 
